8fe85b81939c4e883a91a4a112a08dae693cfd5d
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2023-04-18 17:03:04 +0530

    Initial Commit

---
+++ QuizTime/QuizTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizTimeApp.swift
+//  QuizTime
+//
+//  Created by Bhumika Patel on 18/04/23.
+//
+
+import SwiftUI
+
+@main
+struct QuizTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
