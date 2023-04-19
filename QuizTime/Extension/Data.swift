4dc6e15d4f70a24c2fc701c427cd98ed39e8f68f
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2023-04-19 17:22:09 +0530

    create extension of data encoding all value and catching error for return nil

---
+++ QuizTime/Extension/Data.swift
@@ -0,0 +1,27 @@
+//
+//  Data.swift
+//  QuizTime
+//
+//  Created by Bhumika Patel on 18/04/23.
+//
+
+import Foundation
+
+extension Data {
+    var html2AttributedString: NSAttributedString? {
+        do {
+            return try NSAttributedString(
+                data: self,
+                options: [
+                    .documentType: NSAttributedString.DocumentType.html,
+                    .characterEncoding: String.Encoding.utf8.rawValue
+                ],
+                documentAttributes: nil
+            )
+        } catch {
+            print("error:", error)
+            return  nil
+        }
+    }
+    var html2String: String { html2AttributedString?.string ?? "" }
+}
