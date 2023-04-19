764eec3d5a006aab23eed7722f5a8192e3acdded
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2023-04-19 17:13:25 +0530

    create ectension for padding set cgfloat

---
+++ QuizTime/DesignSystem/DesignSystem.Padding.swift
@@ -0,0 +1,16 @@
+//
+//  DesignSystem.Padding.swift
+//  QuizTime
+//
+//  Created by Bhumika Patel on 18/04/23.
+//
+
+import SwiftUI
+
+extension DesignSystem {
+    struct Padding {
+        static let microPadding: CGFloat = 10
+        static let padding: CGFloat = 10
+        static let macroPadding: CGFloat = 10
+    }
+}
